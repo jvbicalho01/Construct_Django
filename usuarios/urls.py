dcb13e817aa4220add4fce5c4af83e0ac632dad4
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-03-18 14:41:55 -0300

    add login, logout and access control

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar-vendedor/', views.cadastrar_vendedor, name='cadastrar-vendedor')
+    path('cadastrar-vendedor/', views.cadastrar_vendedor, name='cadastrar-vendedor'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rolepermissions.decorators import has_permission_decorator
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import auth
+
+from .models import Users
 
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
-    return HttpResponse("Teste")
+    if request.method == "GET":
+        return render(request, 'cadastrar_vendedor.html')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = Users.objects.filter(email=email)
+
+        if user.exists():
+            # TODO: utilizar messages do Django
+            return HttpResponse('Email já existe')
+    
+        user = Users.objects.create_user(username=email, email=email, password=senha, cargo="V")
+
+        # TODO: Redirecionar com uma mensagem
+        return HttpResponse('Conta criada com sucesso')
+
+def login(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+           return redirect(reverse('plataforma'))
+        return render(request, 'login.html')
+    if request.method == "POST":
+        login = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=login, password=senha)
+
+        if not user:
+            # TODO: Redirecionar com mensagem de erro
+            return HttpResponse("Usuário inválido")
+        
+        auth.login(request, user)
+        return HttpResponse("Usuário logado com sucesso")
+     
+def logout(request):
+    request.session.flush()
+    return redirect(reverse('login'))
